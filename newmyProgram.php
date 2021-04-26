4ad0e27605de1e3d909955d44c0f047b3d919ffe
dpope1997 <77933655+dpope1997@users.noreply.github.com>
2021-04-25 20:34:35 -0400

    da

--- newmyProgram.php
+++ newmyProgram.php
@@ -3,7 +3,8 @@ $q = $_REQUEST["q"];
 $f = fopen("fence.txt", "r");
 while (($line = fgets($f)) !== FALSE) {
    if (strstr($line, $q)) {
-       echo "<li>Your Card Number and Expiration Date: $line"; 
+       $res = explode(',', $line);
+       echo "Your Card Number:". $res[0] ."<br> Expiration Date:". $res[1]
    }   // outputs card+expiration date if either one is found in the line
 }
 
